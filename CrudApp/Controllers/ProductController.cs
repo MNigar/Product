7a5d0173ae0afd9606f5bf4bc7d15e3960ce3975
MNigar <nigar-4t@live.com>
2021-05-10 01:48:15 -0700

    first commit

---
+++ CrudApp/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using CrudApp.Db;
+using CrudApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+        private readonly ProductContext _context;
+        public ProductController(IHostingEnvironment environment, ProductContext context)
+        {
+            _environment = environment;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product model,IFormFile image)
+        {
+            model.Id = Guid.NewGuid();
+            model.Image = image.FileName;
+            if (ModelState.IsValid)
+            {
+                string path = Path.Combine(_environment.WebRootPath, "Image", image.FileName);
+                FileStream fileStream = new FileStream(path, FileMode.CreateNew);
+                await image.CopyToAsync(fileStream);
+                await _context.Products.AddAsync(model);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+                return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CrudApp/Db/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApp.Db
+{
+    public class ProductContext:DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> context) : base(context) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
